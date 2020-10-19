7005385efcd97ff93c9782a8b56faa2cd2f84c03
StoneRiverKS <aisinkakura@keio.jp>
2020-10-20 02:19:51 +0900

    2nd commit

--- 1.4_1_RCTデータの集計とt検定.R
+++ 1.4_1_RCTデータの集計とt検定.R
@@ -1,19 +1,19 @@
-#tidyverseパッケージのインストール
+#1. tidyverseパッケージのインストール
 #実際はtidyverseパッケージに含まれるdplyrパッケージを利用する
 install.packages("tidyverse")
 
 #libraryの呼び出し
 library("tidyverse")
 
-#データの読み込み
+#2. データの読み込み
 email_data = read_csv("http://www.minethatdata.com/Kevin_Hillstrom_MineThatData_E-MailAnalytics_DataMiningChallenge_2008.03.20.csv")
 
-#女性向けデータの削除
+#3. 女性向けデータの削除
 male_df = email_data %>%
   filter(segment!="Womens E-Mail") %>%
   mutate(treatment = if_else(segment=="Mens E-Mail", 1, 0))
 
-#treatmentについてグループ分けし、購入の発生確率と購入額の平均を求める
+#4. treatmentについてグループ分けし、購入の発生確率と購入額の平均を求める
 summary_by_segment = male_df %>%
   group_by(treatment) %>%
   summarise(conversion_rate=mean(conversion),
@@ -21,7 +21,7 @@ summary_by_segment = male_df %>%
             count=n()
             )
 
-#t検定を行う
+#5. t検定を行う
 ##データの準備
 ###treatment=1のデータのspend列を取り出す
 mens_mail = male_df %>%
